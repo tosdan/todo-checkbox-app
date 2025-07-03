bd3fc5a08f3ae48b436e2f74d4774e49b8954cc4
tosdan <3496697+tosdan@users.noreply.github.com>
2025-07-03 13:40:00 +0200

    cleanup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { AppointmentService } from './services/appointment.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    AppointmentService
+
   ]
 };
